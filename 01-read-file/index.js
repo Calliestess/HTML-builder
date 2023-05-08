a3592470d61ef9e78b61f290c30b93eb03c7d6b3
Calliestess <90801166+Calliestess@users.noreply.github.com>
2023-05-08 23:22:53 +0300

    Some changes submit

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,8 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const filePath = path.join('01-read-file', 'text.txt');
-
-fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
+const readStream = fs.createReadStream(filePath);
+readStream.on('data', function(){
+  console.log('data');
 });
